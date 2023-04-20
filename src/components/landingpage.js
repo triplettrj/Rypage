aa8103d4d97ae259f68c90865aa065dec16e0b24
Ryry Triplett <triplettrj@gmail.com>
2023-04-20 12:30:43 +0800

    stlyed

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaInstagram, FaGithub } from 'react-icons/fa';
 import '../landing-page.css';
 
+
 const LandingPage = () => {
   return (
     <div className="landing-page-container">
@@ -11,15 +12,15 @@ const LandingPage = () => {
         <a href="https://www.instagram.com/ryrytriplett" target="_blank" rel="noopener noreferrer" className="social-media-icon-link">
           <FaInstagram size={100} /> 
         </a>
-        <img src="" alt="" />
+
         <a href="https://github.com/triplettrj" target="_blank" rel="noopener noreferrer" className="social-media-icon-link">
           <FaGithub size={100} />
         </a>
       </div>
       <div className="projects-container">
         <div className="project">
-          <h2 className="project-title">Project Title 1</h2>
-          <p className="project-description">Project description goes here</p>
+          <h2 className="project-title">Project spaghetti diagram</h2>
+          <p className="project-description">Project description: </p> <a href="https://github.com/triplettrj/programmingtest/blob/main/README.md">ReadMe</a>
           <a href="https://programmingtest.vercel.app/Loginpage" className="project-link">Link to project</a>
         </div>
         {/*<div className="project">
